402298489a78a905abdac54639ecd4aa3dec9815
theHolyCrawler <theholycrawler2@gmail.com>
2024-09-17 18:39:20 +0100

    .

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import { parseLeague } from "./league";
 import { parsePlayerTables } from "./players";
 import { parsePlayer } from "./player_details";
 import { parseClubInfo } from "./clubinfo";
-export default { parseLeague, parsePlayerTables, parsePlayer, parseClubInfo };
+export { parseLeague, parsePlayerTables, parsePlayer, parseClubInfo };
